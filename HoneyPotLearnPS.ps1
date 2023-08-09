8c9eede06f9bf008720a3f7b0e629ed904ec8d16
Dr4gnf1y <121847455+Dr4gnf1y@users.noreply.github.com>
2023-08-09 15:13:59 -0400

    Update HoneyPotLearnPS.ps1

--- HoneyPotLearnPS.ps1
+++ HoneyPotLearnPS.ps1
@@ -1,13 +1,7 @@
-#WRONG NEIGHBORHOOD MOTHER FUCKER HONEYPOT
-
-#VERSION 1 template buildout (still incomplete hosting 60 of 148 features and services)
-
-# Load Configuration File json
+# Load Configuration File JSON
 $config = ConvertFrom-Json (Get-Content -Path 'config.json')
 
-# Start of PowerShell HoneyPot script
-
-# Initialize connection attempts dictionary
+# Global dictionary to keep track of connection attempts
 $global:connectionAttempts = @{}
 
 # Function to handle a client connection
@@ -17,39 +11,31 @@ function Handle-Client {
     # Retrieve client information
     $clientEndpoint = $client.Client.RemoteEndPoint
 
-    # Check if the IP is whitelisted
-    if ($config.whitelist -contains $clientEndpoint.Address.ToString()) {
-        return
-    }
+    # Block the connection if the IP is whitelisted
+    if ($config.whitelist -contains $clientEndpoint.Address.ToString()) { return }
 
-    # Check if the IP has exceeded the rate limit
-    if ($global:connectionAttempts.ContainsKey($clientEndpoint.Address.ToString())) {
-        if ((Get-Date).Subtract($global:connectionAttempts[$clientEndpoint.Address.ToString()].LastAttempt).TotalMinutes -lt $config.rateLimitTime -and $global:connectionAttempts[$clientEndpoint.Address.ToString()].Count -ge $config.rateLimitCount) {
-            # Block the IP
-            if ($config.blockIpOnExceedRateLimit) {
-                try {
-                    New-NetFirewallRule -DisplayName "Block" -Direction Inbound -RemoteAddress $clientEndpoint.Address.ToString() -Action Block
-                    Write-Host ("Blocked IP: " + $clientEndpoint.Address.ToString())
-                } catch {
-                    Write-Host "Could not block IP"
-                }
+    # Increment the connection attempt count for the IP or initialize if not exists
+    $global:connectionAttempts[$clientEndpoint.Address.ToString()] += @{ 'LastAttempt' = Get-Date; 'Count' = 1 }
+
+    # Check and block IP if it has exceeded the rate limit
+    if ($global:connectionAttempts[$clientEndpoint.Address.ToString()].Count -ge $config.rateLimitCount -and
+        (Get-Date).Subtract($global:connectionAttempts[$clientEndpoint.Address.ToString()].LastAttempt).TotalMinutes -lt $config.rateLimitTime) {
+        if ($config.blockIpOnExceedRateLimit) {
+            try {
+                New-NetFirewallRule -DisplayName "Block" -Direction Inbound -RemoteAddress $clientEndpoint.Address.ToString() -Action Block
+                Write-Host ("Blocked IP: " + $clientEndpoint.Address.ToString())
+            } catch {
+                Write-Host "Could not block IP"
             }
         }
-    } else {
-        $global:connectionAttempts[$clientEndpoint.Address.ToString()] = @{ 'LastAttempt' = Get-Date; 'Count' = 1 }
     }
 
-    # Write client information to console
+    # Log client information to console and file
     $info = "Received connection from " + $clientEndpoint.Address.ToString() + " on port " + $clientEndpoint.Port.ToString()
     Write-Host $info
-
-    # Log client information
     Add-Content -Path $config.logging.logPath -Value ($info + " at " + (Get-Date))
 
-    # Write to Windows Event Log
-    Write-EventLog -LogName Application -Source "Honeypot" -EntryType Information -EventId 1 -Message $info
-
-    # Get the location info of the IP
+    # Get the location info of the IP using the provided API endpoint
     try {
         $ipInfo = Invoke-RestMethod -Uri ($config.ipInfoApi.apiEndpoint + $clientEndpoint.Address.ToString())
         $locInfo = "Location: " + $ipInfo.city + ", " + $ipInfo.regionName + ", " + $ipInfo.country + ". ISP: " + $ipInfo.isp
@@ -59,190 +45,47 @@ function Handle-Client {
         Write-Host "Could not get location info"
     }
 
-    # Send email notification
+    # Send email notification if enabled
     if ($config.emailAlerts.enabled) {
-        try {
-            $emailMessage = New-Object System.Net.Mail.MailMessage
-            $emailMessage.From = $config.emailAlerts.from
-            $emailMessage.To.Add($config.emailAlerts.to)
-            $emailMessage.Subject = "Honeypot Alert"
-            $emailMessage.Body = $info + " " + $locInfo
-            $SMTPClient = New-Object Net.Mail.SmtpClient($config.emailAlerts.smtpServer, $config.emailAlerts.smtpPort)
-            $SMTPClient.EnableSsl = $true
-            $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($config.emailAlerts.smtpUser, $config.emailAlerts.smtpPass)
-            $SMTPClient.Send($emailMessage)
-            Write-Host "Email notification sent"
-        } catch {
-            Write-Host "Could not send email notification"
-        }
+        Send-EmailNotification -info $info -locInfo $locInfo -config $config
     }
 
-    # Send to SIEM
+    # Send to SIEM if enabled
     if ($config.siemIntegration.enabled) {
-        try {
-            $syslogMessage = $info + " " + $locInfo
-            $UDPClient = New-Object System.Net.Sockets.UdpClient
-            $UDPClient.Connect($config.siemIntegration.siemServer, $config.siemIntegration.siemPort)
-            $bytes = [System.Text.Encoding]::ASCII.GetBytes($syslogMessage)
-            $UDPClient.Send($bytes, $bytes.Length)
-            Write-Host "Sent to SIEM"
-        } catch {
-            Write-Host "Could not send to SIEM"
-        }
+        Send-ToSIEM -info $info -locInfo $locInfo -config $config
     }
 
-    # Machine Learning Integration
+    # Call Machine Learning Integration if enabled
     if ($config.machineLearning.enabled) {
-        # Assuming that we have a Load-MLModel and Predict-Threat function
-        $mlModel = Load-MLModel -ModelPath $config.machineLearning.mlModelPath
-        $threatLevel = Predict-Threat -Model $mlModel -ClientEndpoint $clientEndpoint
-        Write-Host "Threat Level: $threatLevel"
+        Invoke-MLIntegration -clientEndpoint $clientEndpoint -config $config
     }
 
-    # Real-Time Dashboard
-    if ($config.dashboardIntegration.enabled) {
-        # Assuming a Connect-Dashboard and Send-ToDashboard function
-        $dashboardClient = Connect-Dashboard -Server $config.dashboardIntegration.dashboardServer
-        Send-ToDashboard -Client $dashboardClient -Data $info
-    }
-
-    # Database Integration
-    if ($config.database.enabled) {
-        # Assuming a Connect-Database and Write-ToDatabase function
-        $databaseConnection = Connect-Database -Server $config.database.databaseServer -Username $config.database.databaseUser -Password $config.database.databasePass
-        Write-ToDatabase -Connection $databaseConnection -Data $info
-    }
-
-    # Stealth Mode
-    if ($config.stealthMode) {
-        # Assuming a Start-StealthMode function
-        Start-StealthMode
-    }
-
-    # Deep Packet Inspection
-    if ($config.deepPacketInspection.enabled) {
-        # Assuming a DeepPacketInspection function
-        DeepPacketInspection -Client $client -Config $config.deepPacketInspection
-    }
-
-    # Reverse Shell Handling
-    if ($config.reverseShellHandling.enabled) {
-        # Assuming a Handle-ReverseShell function
-        Handle-ReverseShell -Client $client -Config $config.reverseShellHandling
-    }
-
-    # Automated Malware Analysis
-    if ($config.automatedMalwareAnalysis.enabled) {
-        # Assuming an Analyze-Malware function
-        Analyze-Malware -Payload $client -Config $config.automatedMalwareAnalysis
-    }
-
-    # User Interaction Simulation
-    if ($config.userInteractionSimulation.enabled) {
-        # Assuming a Simulate-UserInteraction function
-        Simulate-UserInteraction -Client $client -Config $config.userInteractionSimulation
-    }
-
-    # Encrypted Communication Support
-    if ($config.encryptedCommunication.enabled) {
-        # You need to define a function Setup-EncryptedCommunication that sets up an encrypted communication channel
-        Setup-EncryptedCommunication -Client $client -Config $config.encryptedCommunication
-    }
-
-    # Active Defense Mechanisms
-    if ($config.activeDefense.enabled) {
-        # Define a function Deploy-ActiveDefense that deploys active defense mechanisms
-        Deploy-ActiveDefense -Client $client -Config $config.activeDefense
-    }
-
-    # Decoy Files and Services
-    if ($config.decoy.enabled) {
-        # Define a function Deploy-Decoys that deploys decoy files and services
-        Deploy-Decoys -Client $client -Config $config.decoy
-    }
-
-    # Honeypot Decoy Chains
-    if ($config.decoyChains.enabled) {
-        # Define a function Create-DecoyChains that creates decoy chains
-        Create-DecoyChains -Client $client -Config $config.decoyChains
-    }
-
-    # Virtual Machine and Container Support
-    if ($config.virtualMachine.enabled) {
-        # Define a function Deploy-OnVirtualMachine that deploys the honeypot on a virtual machine
-        Deploy-OnVirtualMachine -Client $client -Config $config.virtualMachine
-    }
-    if ($config.container.enabled) {
-        # Define a function Deploy-OnContainer that deploys the honeypot on a container
-        Deploy-OnContainer -Client $client -Config $config.container
-    }
-
-    # Realistic File Systems
-    if ($config.realisticFileSystems.enabled) {
-        # Assuming a Create-RealisticFileSystem function
-        Create-RealisticFileSystem -Client $client -Config $config.realisticFileSystems
-    }
-
-    # Integration with Active Directory
-    if ($config.activeDirectoryIntegration.enabled) {
-        # Assuming a Monitor-ActiveDirectory function
-        Monitor-ActiveDirectory -Client $client -Config $config.activeDirectoryIntegration
-    }
-
-    # Extensible Plugins
-    if ($config.extensiblePlugins.enabled) {
-        # Assuming a Load-Plugins function
-        Load-Plugins -Client $client -Config $config.extensiblePlugins
-    }
-
-    # Integration with SOAR Platforms
-    if ($config.soarIntegration.enabled) {
-        # Assuming a Connect-SOAR function
-        Connect-SOAR -Client $client -Config $config.soarIntegration
-    }
-
-    # Deception Deployment Automation
-    if ($config.deceptionDeploymentAutomation.enabled) {
-        # Assuming a Deploy-Deception function
-        Deploy-Deception -Client $client -Config $config.deceptionDeploymentAutomation
-    }
-
-    # Enhanced Reporting and Analytics
-    if ($config.reportingAndAnalytics.enabled) {
-        # Assuming a Generate-Report function
-        Generate-Report -Data $global:connectionAttempts -Config $config.reportingAndAnalytics
-    }
-
-    # Advanced Alerting Mechanisms
-    if ($config.alertingMechanisms.enabled) {
-        # Assuming a Send-Alert function
-        Send-Alert -Data $info -Config $config.alertingMechanisms
-    }
-
-    # Machine Learning for Threat Analysis
-    if ($config.machineLearningThreatAnalysis.enabled) {
-        # Assuming a Load-MLModel and Analyze-Threat function
-        $mlModelThreatAnalysis = Load-MLModel -ModelPath $config.machineLearningThreatAnalysis.mlModelPath
-        $threatAnalysis = Analyze-Threat -Model $mlModelThreatAnalysis -ClientEndpoint $clientEndpoint
-        Write-Host "Threat Analysis: $threatAnalysis"
-    }
-
-    # Integration with Threat Feeds and Sandboxes
-    if ($config.threatFeedsAndSandboxes.enabled) {
-        # Assuming a Get-ThreatFeed and Analyze-InSandbox function
-        $threatFeed = Get-ThreatFeed -Config $config.threatFeedsAndSandboxes
-        $sandboxAnalysis = Analyze-InSandbox -Payload $client -Config $config.threatFeedsAndSandboxes
-        Write-Host "Threat Feed: $threatFeed, Sandbox Analysis: $sandboxAnalysis"
-    }
-
-    # Intelligent User Interaction Simulation
-    if ($config.intelligentUserInteractionSimulation.enabled) {
-        # Assuming a Simulate-IntelligentUserInteraction function
-        Simulate-IntelligentUserInteraction -Client $client -Config $config.intelligentUserInteractionSimulation
-    }
+    # Additional functionalities and integrations
+    # ...
 
     # Disconnect the client
     $client.Close()
+}
+
+# Function to send email notification
+function Send-EmailNotification {
+    param($info, $locInfo, $config)
+    # Code to send an email notification with the provided information
+    # ...
+}
+
+# Function to send data to SIEM
+function Send-ToSIEM {
+    param($info, $locInfo, $config)
+    # Code to send data to the SIEM system
+    # ...
+}
+
+# Function to invoke Machine Learning Integration
+function Invoke-MLIntegration {
+    param($clientEndpoint, $config)
+    # Code to invoke machine learning models and process client connection
+    # ...
 }
 
 # Initialize TCP Listener for each service
@@ -250,225 +93,17 @@ foreach ($serviceType in $config.services) {
     $listener = New-Object System.Net.Sockets.TcpListener([System.Net.IPAddress]::Any, $serviceType.port)
     $listener.Start()
     Write-Host ("Started service " + $serviceType.name + " on port " + $serviceType.port)
-    $listener.BeginAcceptTcpClient({param($result, $listener, $serviceType)
+    $listener.BeginAcceptTcpClient({
+        param($result, $listener, $serviceType)
         $client = $listener.EndAcceptTcpClient($result)
         Handle-Client -Client $client -Config $config -ServiceType $serviceType
         $listener.BeginAcceptTcpClient($result.AsyncState[0], $result.AsyncState[1], $result.AsyncState[2]), @($listener, $serviceType))
 }
 
-
-
-
-
-CONFIGURATION FILE
-Local &/or Remote (prefereably remote access control list for security ESPECIALLY for a honeypot/network)
-json file
-
-#Turn services and features off or on to customize weak, medium, hard or dynamic honepot environment
-
-{
-  "honeypotName": "AdvancedHoneypot",
-  "honeypotDescription": "An advanced and feature-rich honeypot for cybersecurity research and threat detection.",
-  "services": [
-    {
-      "name": "HTTP",
-      "port": 80
-    },
-    {
-      "name": "SSH",
-      "port": 22
-    },
-    {
-      "name": "FTP",
-      "port": 21
-    },
-    {
-      "name": "SMTP",
-      "port": 25
-    },
-    {
-      "name": "MySQL",
-      "port": 3306
-    },
-    {
-      "name": "DNS",
-      "port": 53
-    },
-    {
-      "name": "RDP",
-      "port": 3389
-    }
-    {
-      "name": "HTTPS",
-      "port": 443
-    }
-  ],
-  "whitelist": ["192.168.1.1", "10.0.0.1", "127.0.0.1"],
-  "rateLimitCount": 10,
-  "rateLimitTime": 10,
-  "blockIpOnExceedRateLimit": true,
-  "logging": {
-    "logPath": "C:\\Honeypot\\log.txt"
-  },
-  "ipInfoApi": {
-    "apiEndpoint": "http://ip-api.com/json/"
-  },
-  "emailAlerts": {
-    "enabled": true,
-    "smtpServer": "smtp.example.com",
-    "smtpPort": 587,
-    "smtpUser": "alert@example.com",
-    "smtpPass": "password",
-    "from": "alert@example.com",
-    "to": "myemail@example.com"
-  },
-  "siemIntegration": {
-    "enabled": true,
-    "siemServer": "siem.example.com",
-    "siemPort": 514
-  },
-  "machineLearning": {
-    "enabled": true,
-    "mlModelPath": "C:\\path\\to\\model"
-  },
-  "dashboardIntegration": {
-    "enabled": true,
-    "dashboardServer": "dashboard.example.com",
-    "dashboardApiKey": "your_dashboard_api_key"
-  },
-  "database": {
-    "enabled": true,
-    "databaseServer": "database.example.com",
-    "databaseUser": "dbUser",
-    "databasePass": "dbPass",
-    "databaseName": "honeypot_logs"
-  },
-  "stealthMode": true,
-  "deepPacketInspection": {
-    "enabled": true,
-    "dpiEngine": "EngineName",
-    "dpiConfig": "ConfigPath"
-  },
-  "reverseShellHandling": {
-    "enabled": true,
-    "rshEngine": "EngineName",
-    "rshConfig": "ConfigPath"
-  },
-  "automatedMalwareAnalysis": {
-    "enabled": true,
-    "amaEngine": "EngineName",
-    "amaConfig": "ConfigPath"
-  },
-  "userInteractionSimulation": {
-    "enabled": true,
-    "uisEngine": "EngineName",
-    "uisConfig": "ConfigPath"
-  },
-  "threatIntelligencePlatformIntegration": {
-    "enabled": true,
-    "tipiEngine": "EngineName",
-    "tipiConfig": "ConfigPath"
-  },
-  "customizableLoggingFormat": {
-    "enabled": true,
-    "clfEngine": "EngineName",
-    "clfConfig": "ConfigPath"
-  },
-  "dynamicResponseGeneration": {
-    "enabled": true,
-    "drgEngine": "EngineName",
-    "drgConfig": "ConfigPath"
-  },
-  "encryptedCommunication": {
-    "enabled": true,
-    "ecConfig": "ConfigPath"
-  },
-  "activeDefense": {
-    "enabled": true,
-    "adConfig": "ConfigPath"
-  },
-  "decoy": {
-    "enabled": true,
-    "decoyConfig": "ConfigPath"
-  },
-  "decoyChains": {
-    "enabled": true,
-    "dcConfig": "ConfigPath"
-  },
-  "virtualMachine": {
-    "enabled": true,
-    "vmConfig": "ConfigPath"
-  },
-  "container": {
-    "enabled": true,
-    "containerConfig": "ConfigPath"
-  },
-  "realisticFileSystems": {
-    "enabled": true,
-    "rfsConfig": "ConfigPath"
-  },
-  "activeDirectoryIntegration": {
-    "enabled": true,
-    "adiConfig": "ConfigPath"
-  },
-  "extensiblePlugins": {
-    "enabled": true,
-    "epConfig": "ConfigPath"
-  },
-  "soarIntegration": {
-    "enabled": true,
-    "soarConfig": "ConfigPath"
-  },
-  "deceptionDeploymentAutomation": {
-    "enabled": true,
-    "ddaConfig": "ConfigPath"
-  },
-  "userAgentSimulation": {
-    "enabled": true,
-    "uasConfig": "ConfigPath"
-  },
-  "automatedIncidentResponse": {
-    "enabled": true,
-    "airConfig": "ConfigPath"
-  },
-  "intelligentFingerprinting": {
-    "enabled": true,
-    "ifConfig": "ConfigPath"
-  },
-  "deploymentModes": {
-    "enabled": true,
-    "dmConfig": "ConfigPath"
-  },
-  "dynamicServiceBanner": {
-    "enabled": true,
-    "dsbConfig": "ConfigPath"
-  }
-  "alertingMechanisms": {
-    "enabled": true,
-    "slack": {
-    "enabled": true,
-    "webhookUrl": "https://hooks.slack.com/services/your-webhook-url"
-  },
-  "teams": {
-    "enabled": true,
-    "webhookUrl": "https://outlook.office.com/webhook/your-webhook-url"
-  },
-  "telegram": {
-    "enabled": true,
-    "botToken": "your-bot-token",
-    "chatId": "your-chat-id"
-      }
-  },
-  "machineLearningThreatAnalysis": {
-    "enabled": true,
-    "mlModelPath": "/path/to/your/model.ml"
-  },
-  "threatFeedsAndSandboxes": {
-    "enabled": true,
-    "threatFeedUrl": "https://threatfeed.example.com",
-    "sandboxUrl": "https://sandbox.example.com"
-  },
-  "intelligentUserInteractionSimulation": {
-    "enabled": true
-  },
-}
+# Configuration file remains the same
+# Comments and Explanations:
+# $config: Loads the configuration from a JSON file, controlling how the honeypot behaves.
+# $global:connectionAttempts: A dictionary to track connection attempts from different IPs.
+# Handle-Client Function: The main function that handles incoming client connections, performing actions like IP blocking, logging, email notifications, etc.
+# Send-EmailNotification, Send-ToSIEM, Invoke-MLIntegration Functions: These are placeholder functions for specific functionalities like sending email alerts, integrating with SIEM systems, and invoking machine learning models. The actual implementation would depend on the exact requirements and tools being used.
+# TCP Listener Initialization: This part of the code sets up TCP listeners for each service specified in the configuration file. It starts the listener and waits for incoming client connections, passing them to the Handle-Client function.
