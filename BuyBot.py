e69bf578bad3de1f900bf2fba8517aef3c244152
Dr4gnf1y <121847455+Dr4gnf1y@users.noreply.github.com>
2023-09-17 03:37:59 -0400

    Update BuyBot.py

--- BuyBot.py
+++ BuyBot.py
@@ -1,17 +1,17 @@
 # Import all required libraries and modules
-from flask import Flask, jsonify, request
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask import Flask, jsonify, request, abort
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from redis import Redis
-from celery import Celery
-import requests
+from celery import Celery, group
 from pymongo import MongoClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Session
+from sqlalchemy.pool import QueuePool
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error
-from python_anticaptcha import AnticaptchaClient
+from python_anticaptcha import AnticaptchaClient, NoCaptchaTaskProxyless, ImageToTextTask
 import hashlib
 import logging.config
 import logging.handlers
@@ -26,6 +26,22 @@ from selenium import webdriver
 import threading
 from twilio.rest import Client
 import smtplib
+from requests.exceptions import HTTPError, Timeout, RequestException
+from datetime import datetime
+import backoff
+import asyncio
+import websockets
+import json
+from pika import BlockingConnection, ConnectionParameters, PlainCredentials
+from queue import Queue
+from cachetools import cached, TTLCache
+from marshmallow import Schema, fields
+from itertools import cycle
+import pandas as pd
+from requests.sessions import Session
+from win10toast import ToastNotifier
+import os
+from time import sleep
 
 # Initialize Flask application
 flask_app = Flask(__name__)
@@ -34,6 +50,7 @@ flask_app = Flask(__name__)
 logger = logging.getLogger('python-logstash-logger')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.handlers.TimedRotatingFileHandler('my_log.log', when="midnight", interval=1, backupCount=10))
+logger.addHandler(logging.handlers.TimedRotatingFileHandler('auto_scaling.log', when="midnight", interval=1, backupCount=10))
 
 # Initialize Redis for rate limiting and as a message broker for Celery
 redis = Redis(host='localhost', port=6379, db=0)
@@ -62,6 +79,61 @@ login_manager.init_app(flask_app)
 
 # Global analytics data
 analytics_data = {'tasks': 0, 'success': 0, 'fail': 0}
+
+# Adaptive sleep time
+adaptive_sleep_time = 1  # in seconds
+MIN_SLEEP_TIME = 0.5  # The minimum time to sleep in seconds
+MAX_SLEEP_TIME = 5.0  # The maximum time to sleep in seconds
+SUCCESS_DECREASE_FACTOR = 0.9  # Factor to decrease sleep time on success
+FAILURE_INCREASE_FACTOR = 1.5  # Factor to increase sleep time on failure or rate limit
+
+# Simulated geographic locations of the servers (latitude, longitude)
+geo_locations = {
+    'us_east': (40.7128, -74.0060),
+    'us_west': (37.7749, -122.4194),
+    'eu': (52.5200, 13.4050)
+}
+
+# Define the user's current geographic location (for simulation)
+user_location = (39.9042, 116.4074)  # Beijing for example
+
+# New Celery task for making a purchase with adaptive sleep and geolocation
+@app.task
+def make_purchase(product_id, user_geo_location=user_location):
+    global adaptive_sleep_time
+
+    # Determine the closest geo-location for the server
+    closest_server = min(geo_locations, key=lambda x: ((user_geo_location[0] - geo_locations[x][0]) ** 2 + (user_geo_location[1] - geo_locations[x][1]) ** 2) ** 0.5)
+    logger.info(f"Closest server for the task is {closest_server}")
+
+    # Sleep for adaptive_sleep_time seconds before making a request
+    sleep(adaptive_sleep_time)
+
+    if not token_bucket_request():
+        analytics_data['fail'] += 1
+        logger.warning(f"Rate limit exceeded for product {product_id} on {closest_server} server")
+        adaptive_sleep_time = min(MAX_SLEEP_TIME, adaptive_sleep_time * FAILURE_INCREASE_FACTOR)
+        return
+
+    analytics_data['tasks'] += 1
+    masked_product_id = hashlib.sha256(str(product_id).encode()).hexdigest()
+    response = requests.get(f'https://api.example.com/products/{masked_product_id}')
+
+    if response.status_code == 200:
+        features = [1, 2, 3, 4]
+        # Call your get_decision function here and use the returned decision
+        decision = 1  # Replace with actual decision value
+        if decision > 0.5:
+            analytics_data['success'] += 1
+            logger.info(f"Successfully made a decision for product {product_id}")
+            adaptive_sleep_time = max(MIN_SLEEP_TIME, adaptive_sleep_time * SUCCESS_DECREASE_FACTOR)
+        else:
+            analytics_data['fail'] += 1
+            logger.warning(f"Failed to make a decision for product {product_id}")
+    else:
+        analytics_data['fail'] += 1
+        logger.error(f"Failed to fetch product {product_id}")
+        adaptive_sleep_time = min(MAX_SLEEP_TIME, adaptive_sleep_time * FAILURE_INCREASE_FACTOR)
 
 # Define a user class for Flask-Login
 class User(UserMixin):
@@ -160,3 +232,369 @@ def logout():
 # Flask main loop
 if __name__ == '__main__':
     flask_app.run(debug=True)
+
+# Define a function to handle backoff retries
+@backoff.on_exception(backoff.expo, (HTTPError, Timeout, RequestException), max_tries=8)
+def safe_requests_get(url):
+    try:
+        return requests.get(url)
+    except (HTTPError, Timeout, RequestException) as e:
+        logger.error(f"Error encountered while requesting {url}: {str(e)}")
+        raise
+
+# Improved Real-Time Monitoring Function
+def real_time_monitoring(item_url, max_retries=3, initial_sleep_time=1, max_sleep_time=60):
+    retries = 0
+    sleep_time = initial_sleep_time
+    while True:
+        try:
+            # Perform a health check before making a request
+            health_check()
+
+            # Use the safe_requests_get function to make the GET request with retries
+            response = safe_requests_get(item_url)
+
+            # Validate HTTP Response
+            response.raise_for_status()
+
+            # Check item availability
+            if is_item_available(response.text):
+                logger.info("Item is available!")
+            else:
+                logger.warning("Item is not available!")
+
+        except (HTTPError, Timeout, RequestException):
+            logger.error(f"Failed to check item at {item_url}, retrying...")
+            retries += 1
+            if retries > max_retries:
+                logger.error("Max retries reached, stopping the monitor.")
+                return
+
+            sleep_time = min(sleep_time * 2, max_sleep_time)
+            time.sleep(sleep_time)
+
+# Define a function for the health check
+def health_check():
+    status = {
+        "API": None,
+        "MongoDB": None,
+        "SQLite": None,
+        "PostgreSQL": None,
+        "Redis": None,
+        "System": None
+    }
+
+    # Check API availability
+    try:
+        response = requests.get('https://api.example.com/health')
+        status["API"] = response.status_code == 200
+    except Exception as e:
+        status["API"] = False
+        logger.error(f"API Health Check Failed: {e}")
+
+    # Check MongoDB
+    try:
+        status["MongoDB"] = mongo_client.server_info() is not None
+    except Exception as e:
+        status["MongoDB"] = False
+        logger.error(f"MongoDB Health Check Failed: {e}")
+
+    # Check SQLite
+    try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1")
+        conn.close()
+        status["SQLite"] = True
+    except Exception as e:
+        status["SQLite"] = False
+        logger.error(f"SQLite Health Check Failed: {e}")
+
+    # Check PostgreSQL
+    try:
+        conn = psycopg2.connect(database="dbname", user="username", password="password", host="localhost")
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1")
+        conn.close()
+        status["PostgreSQL"] = True
+    except Exception as e:
+        status["PostgreSQL"] = False
+        logger.error(f"PostgreSQL Health Check Failed: {e}")
+
+    # Check Redis
+    try:
+        redis.ping()
+        status["Redis"] = True
+    except Exception as e:
+        status["Redis"] = False
+        logger.error(f"Redis Health Check Failed: {e}")
+
+    # Check System Health (Disk Space, CPU Usage, etc.)
+    try:
+        disk_space = os.statvfs('/')
+        disk_free = disk_space.f_frsize * disk_space.f_bavail
+        status["System"] = disk_free > 1000000  # More than 1MB free disk space
+    except Exception as e:
+        status["System"] = False
+        logger.error(f"System Health Check Failed: {e}")
+
+    return status
+
+# Define a function to check item availability
+def is_item_available(html_content):
+    # Implement your logic to check if the item is available based on the HTML content
+    # For example, you can use BeautifulSoup to parse the HTML and look for a specific element
+    return False
+
+# Run the real-time monitoring function
+real_time_monitoring("https://www.example.com/item/123")
+
+# Remaining code that includes UserProfile, api_failover, and other functionalities
+class UserProfile:
+    def __init__(self, user_id, strategy, payment_method, preferences):
+        self.user_id = user_id
+        self.strategy = strategy  # 'aggressive', 'moderate', 'conservative'
+        self.payment_method = payment_method  # 'credit_card', 'paypal', etc.
+        self.preferences = preferences  # Any other custom preferences like 'category_pref': 'electronics'
+
+    def get_decision_threshold(self):
+        if self.strategy == 'aggressive':
+            return 0.4
+        elif self.strategy == 'moderate':
+            return 0.5
+        else:  # conservative
+            return 0.6
+
+    def get_payment_details(self):
+        return "Payment details here"
+
+    def get_custom_features(self):
+        return [1, 2, 3, 4]  # Replace with actual features based on preferences
+
+# API failover using Selenium
+def api_failover(product_id):
+    try:
+        driver = webdriver.Chrome()
+        driver.get(f"https://example.com/products/{product_id}")
+        product_data = driver.find_element_by_id("product-info").text
+        driver.quit()
+        return product_data
+    except Exception as e:
+        logger.error(f"Selenium WebDriver Error: {e}")
+        return None
+
+# Celery task for making a purchase with failover
+@app.task
+def make_purchase_with_failover(product_id):
+    if not token_bucket_request():
+        analytics_data['fail'] += 1
+        logger.warning(f"Rate limit exceeded for product {product_id}")
+        return
+
+    analytics_data['tasks'] += 1
+    masked_product_id = mask_data(str(product_id))
+
+    try:
+        response = requests.get(f'https://api.example.com/products/{masked_product_id}')
+        response.raise_for_status()
+    except RequestException as e:
+        logger.warning(f"API request failed: {e}. Initiating failover mechanism.")
+        scraped_data = api_failover(product_id)
+        
+        if scraped_data is None:
+            analytics_data['fail'] += 1
+            logger.error(f"Failover mechanism failed for product {product_id}")
+            return
+        else:
+            features = [1, 2, 3, 4]  # Replace with actual features based on scraped data
+    else:
+        features = [1, 2, 3, 4]  # Replace with actual features based on API data
+
+    decision = get_decision(features)
+    
+    if decision > 0.5:
+        analytics_data['success'] += 1
+        logger.info(f'Successfully made a decision for product {product_id}')
+    else:
+        analytics_data['fail'] += 1
+        logger.warning(f'Failed to make a decision for product {product_id}')
+
+# Additional health check endpoint
+@flask_app.route('/health', methods=['GET'])
+def health_endpoint():
+    status = health_check()
+    if all(status.values()):
+        return jsonify({"status": "Healthy", "details": status}), 200
+    else:
+        return jsonify({"status": "Unhealthy", "details": status}), 500
+
+# Audit trail functionality
+def create_audit_trail(action, status, user_id=None, extra_info=None):
+    audit_data = {
+        "timestamp": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z',
+        "action": action,
+        "status": status,
+        "user_id": user_id,
+        "extra_info": extra_info
+    }
+    audit_json = json.dumps(audit_data, sort_keys=True)
+    audit_hash = hashlib.sha256(audit_json.encode()).hexdigest()
+    audit_data["hash"] = audit_hash
+    logger.info(f"Audit Trail: {audit_data}")
+
+    try:
+        audit_collection = mongo_db['audit_trails']
+        audit_collection.insert_one(audit_data)
+    except Exception as e:
+        logger.error(f"Failed to insert audit trail into MongoDB: {e}")
+
+    return audit_data["hash"]
+
+# Flask route for audit trails
+@flask_app.route('/audit_trails', methods=['GET'])
+@login_required
+def get_audit_trails():
+    audit_collection = mongo_db['audit_trails']
+    audits = list(audit_collection.find({}))
+    return jsonify(audits), 200
+
+# User feedback collection
+@flask_app.route('/collect_feedback', methods=['POST'])
+@login_required
+def collect_user_feedback():
+    try:
+        feedback_data = request.json
+        action = feedback_data.get('action')
+        feedback = feedback_data.get('feedback')
+        extra_info = feedback_data.get('extra_info')
+        feedback_doc = {
+            'action': action,
+            'feedback': feedback,
+            'extra_info': extra_info,
+            'timestamp': datetime.utcnow()
+        }
+        feedback_collection = mongo_db['feedback']
+        feedback_collection.insert_one(feedback_doc)
+        return jsonify({"status": "Feedback successfully collected"}), 200
+    except Exception as e:
+        logger.error(f"Failed to collect feedback: {e}")
+        abort(500, description="Internal Server Error")
+
+# Function to retrain ML model with feedback
+def retrain_model_with_feedback():
+    feedback_data = list(feedback_collection.find({}))
+    X_new = []
+    y_new = []
+    for feedback in feedback_data:
+        features = feedback['extra_info']['features']
+        label = feedback['feedback']
+        X_new.append(features)
+        y_new.append(label)
+
+    X_old, y_old = make_regression(n_samples=100, n_features=4, noise=0.1)
+    X_combined = X_old + X_new
+    y_combined = y_old + y_new
+
+    model = RandomForestRegressor(n_estimators=100, max_depth=2)
+    model.fit(X_combined, y_combined)
+    logger.info("Model retrained with user feedback.")
+
+# Celery task for auto-scaling
+@app.task
+def auto_scale_workers():
+    global current_workers
+    pending_tasks = len(app.control.inspect().scheduled()['celery@worker1'])
+    if pending_tasks > current_workers * SCALING_FACTOR:
+        current_workers = min(MAX_WORKERS, current_workers * SCALING_FACTOR)
+    elif pending_tasks < current_workers / SCALING_FACTOR:
+        current_workers = max(MIN_WORKERS, current_workers / SCALING_FACTOR)
+    logger.info(f"Auto-scaling: adjusted worker count to {current_workers}")
+
+# Example of triggering auto-scaling before dispatching tasks
+@app.task
+def dispatch_tasks():
+    auto_scale_workers()
+    task_ids = range(100)
+    job = group(make_purchase.s(i) for i in task_ids)
+    result = job.apply_async()
+
+# Modify your existing logging setup
+logger.addHandler(logging.handlers.TimedRotatingFileHandler('auto_scaling.log', when="midnight", interval=1, backupCount=10))
+
+
+
+from time import sleep
+adaptive_sleep_time = 1  # in seconds
+MIN_SLEEP_TIME = 0.5  # The minimum time to sleep in seconds
+MAX_SLEEP_TIME = 5.0  # The maximum time to sleep in seconds
+SUCCESS_DECREASE_FACTOR = 0.9  # Factor to decrease sleep time on success
+FAILURE_INCREASE_FACTOR = 1.5  # Factor to increase sleep time on failure or rate limit
+
+@app.task
+def make_purchase(product_id):
+    global adaptive_sleep_time  # Declare it as global to modify the value
+
+    # Sleep for adaptive_sleep_time seconds before making a request
+    sleep(adaptive_sleep_time)
+
+    if not token_bucket_request():
+        analytics_data['fail'] += 1
+        logger.warning(f"Rate limit exceeded for product {product_id}")
+        
+        # Increase adaptive_sleep_time due to rate limit
+        adaptive_sleep_time = min(MAX_SLEEP_TIME, adaptive_sleep_time * FAILURE_INCREASE_FACTOR)
+        return
+
+    analytics_data['tasks'] += 1
+    masked_product_id = mask_data(str(product_id))
+    response = requests.get(f'https://api.example.com/products/{masked_product_id}')
+
+    if response.status_code == 200:
+        features = [1, 2, 3, 4]  # Replace with actual features
+        decision = get_decision(features)
+        if decision > 0.5:
+            analytics_data['success'] += 1
+            logger.info(f"Successfully made a decision for product {product_id}")
+
+            # Decrease adaptive_sleep_time due to successful request
+            adaptive_sleep_time = max(MIN_SLEEP_TIME, adaptive_sleep_time * SUCCESS_DECREASE_FACTOR)
+        else:
+            analytics_data['fail'] += 1
+            logger.warning(f"Failed to make a decision for product {product_id}")
+    else:
+        analytics_data['fail'] += 1
+        logger.error(f"Failed to fetch product {product_id}")
+
+        # Increase adaptive_sleep_time due to failed request
+        adaptive_sleep_time = min(MAX_SLEEP_TIME, adaptive_sleep_time * FAILURE_INCREASE_FACTOR)
+
+
+
+# Simulated geographic locations of the servers (latitude, longitude)
+geo_locations = {
+    'us_east': (40.7128, -74.0060),
+    'us_west': (37.7749, -122.4194),
+    'eu': (52.5200, 13.4050)
+}
+
+# Define the user's current geographic location (for simulation)
+user_location = (39.9042, 116.4074)  # Beijing for example
+
+def simulated_geo_distance(user_loc, server_loc):
+    # Simulate the behavior of geopy.distance.distance
+    # For this example, a simple Euclidean distance is used.
+    return ((user_loc[0] - server_loc[0]) ** 2 + (user_loc[1] - server_loc[1]) ** 2) ** 0.5
+
+    @app.task
+def make_purchase(product_id, user_geo_location=user_location):
+    # Determine the closest geo-location for the server
+    closest_server = min(geo_locations, key=lambda x: simulated_geo_distance(user_geo_location, geo_locations[x]))
+
+    # Log the closest server
+    logger.info(f"Closest server for the task is {closest_server}")
+
+    # Implement the rest of your existing logic here
+    if not token_bucket_request():
+        analytics_data['fail'] += 1
+        logger.warning(f"Rate limit exceeded for product {product_id} on {closest_server} server")
+        return
