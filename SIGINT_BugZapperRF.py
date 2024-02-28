ce6f12299146916636a46171e429a5b78b719dd1
Dr4gnf1y <121847455+Dr4gnf1y@users.noreply.github.com>
2024-02-28 01:16:24 -0600

    Update and rename OSINT_BugZapperRF.py to SIGINT_BugZapperRF.py

--- SIGINT_BugZapperRF.py
+++ SIGINT_BugZapperRF.py
@@ -1,7 +1,3 @@
-# While technically yes this would fall into SIGINT I labeled these as I am using open source information to FIND all this stuff.
-# Sorry for the confusion it might cause some. I'm literally just trying to create open source information not pick through the weeds
-
-
 ## UAV Emergency Landing Command (2013 from Violent Python)
 import threading
 import dup
